e0a3561075cc54b78f62110d17945c0d28f21ae6
Alvaro Ponguta <alvaroponguta.ponguta@wnco.com>
2020-06-11 10:34:30 -0500

    Refactor code based on code review comments

--- NodeJS_Course/countries-app/app.js
+++ NodeJS_Course/countries-app/app.js
@@ -1,15 +1,34 @@
-const request = require('request');
+const axios = require('axios');
+const yargs = require('yargs');
 
-const country = 'Colombia';
-const url = `https://restcountries.eu/rest/v2/name/${country}`;
-
-request({ url, json: true }, (error, response) => {
-    if (error) {
-        console.log('No se puedo establecer una conexion con restcountries');
-    } else if (!response.body) {
-        console.log('Hubo un error, revise sus parametros y vuelva a intentar');
-    } else {
-        const countryData = response.body[0];
-        console.log(`La capital de ${country} es ${countryData.capital}`);
-    }
+const axiosInstance = axios.create({
+    baseURL: 'https://restcountries.eu/rest/v2/',
+    timeout: 1000
 });
+
+yargs.command({
+    command: 'capital',
+    describe: 'Get the capital of an specific country',
+    builder: {
+        countryName: {
+            describe: 'Name of the country',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: country => getCapital(country.countryName)
+});
+
+const getCapital = country => {
+    axiosInstance.get(`name/${country}`)
+        .then(response => {
+            const { capital } = response.data[0];
+
+            console.log(`La capital de ${country} es ${capital}`);
+        })
+        .catch(error => {
+            console.error(`Hubo un error ${error.response.status}, por favor revise sus parametros y vuelva a intentarlo`);
+        });
+}
+
+yargs.parse();
