c12889ece25d7a2b1c63da1910b52e84934861b1
Alvaro Ponguta <alvaroponguta.ponguta@wnco.com>
2020-06-19 14:02:30 -0500

    Add files management

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = new express.Router();
+const multer = require('multer');
 const User = require('../models/user');
 const auth = require('../middlewares/auth');
+const storage = multer.memoryStorage();
+const sharp = require('sharp');
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
@@ -77,6 +80,45 @@ router.delete('/users/me', auth, async (req, res) => {
     } catch (error) {
         res.status(500).send(error);
     }
+});
+
+const upload = multer({
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('File must be a JPG, JPEG or PNG'));
+        }
+
+        cb(undefined, true);
+    },
+    storage
+});
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({
+        width: 250,
+        height: 250
+    }).png().toBuffer();
+
+    req.user.avatar = buffer;
+    await req.user.save();
+    res.send();
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+});
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send();
+});
+
+router.get('/users/me/avatar', auth, async (req, res) => {
+    res.set('Content-Type', 'image/png');
+    res.send(req.user.avatar);
 });
 
 module.exports = router;
